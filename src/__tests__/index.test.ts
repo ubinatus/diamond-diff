8733736aae81641b93fd0cad873e2d2fea2e09d1
ubinatus <ja.castrocabrera@gmail.com>
2022-09-10 17:35:25 -0500

    test: more complex diamond diff

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -226,23 +226,125 @@ describe("Desired cut", () => {
     ];
     const diamondCut = ensureDiamondFacets(current, model);
 
-    expect(diamondCut).toEqual([
+    expect(diamondCut).toEqual(
+      expect.arrayContaining([
+        {
+          facetAddress: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+          action: FacetCutAction.Add,
+          functionSelectors: ["0x5a150cb7", "0x11bd8a76", "0x6839c886"],
+        },
+        {
+          facetAddress: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+          action: FacetCutAction.Replace,
+          functionSelectors: ["0x8da5cb5b", "0xf2fde38b"],
+        },
+        {
+          facetAddress: AddressZero,
+          action: FacetCutAction.Remove,
+          functionSelectors: ["0x52ef6b2c"],
+        },
+      ])
+    );
+  });
+  it("returns desired cut when a complex add, remove are replaced are required", () => {
+    const current = [
       {
-        facetAddress: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
-        action: FacetCutAction.Replace,
+        facetAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+        functionSelectors: ["0xcdffacc6", "0x52ef6b2c"],
+      },
+      {
+        facetAddress: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
         functionSelectors: ["0x8da5cb5b", "0xf2fde38b"],
       },
       {
-        facetAddress: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
-        action: FacetCutAction.Add,
-        functionSelectors: ["0x5a150cb7", "0x11bd8a76", "0x6839c886"],
+        facetAddress: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
+        functionSelectors: ["0xaa6ca808", "0xff70174a", "0x06fdde03"],
       },
+    ];
+    const model = [
+      // Changes facet address, removes selector, adds selector
       {
-        facetAddress: AddressZero,
-        action: FacetCutAction.Remove,
-        functionSelectors: ["0x52ef6b2c"],
+        // facetAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+        facetAddress: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+        // functionSelectors: ["0xcdffacc6", "0x52ef6b2c"],
+        functionSelectors: ["0xcdffacc6", "0x4290f136"],
       },
-    ]);
+      // Removes selector
+      {
+        facetAddress: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+        // functionSelectors: ["0x8da5cb5b", "0xf2fde38b"],
+        functionSelectors: ["0x8da5cb5b"],
+      },
+      // Adds selector
+      {
+        facetAddress: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
+        // functionSelectors: ["0xaa6ca808", "0xff70174a", "0x06fdde03"],
+        functionSelectors: [
+          "0xaa6ca808",
+          "0xff70174a",
+          "0x06fdde03",
+          "0x95d89b41",
+        ],
+      },
+      // Adds new functions
+      {
+        facetAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+        functionSelectors: [
+          "0x70a08231",
+          "0x78130407",
+          "0x313ce567",
+          "0x569a03bf",
+          "0xa04ef427",
+          "0x3223af9d",
+          "0xfdff9b4d",
+          "0x08ae4b0c",
+          "0xe2f67dcd",
+          "0xe54ffe8c",
+        ],
+      },
+    ];
+    const diamondCut = ensureDiamondFacets(current, model);
+    console.log(diamondCut);
+    expect(diamondCut).toEqual(
+      expect.arrayContaining([
+        {
+          facetAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+          action: FacetCutAction.Add,
+          functionSelectors: [
+            "0x70a08231",
+            "0x78130407",
+            "0x313ce567",
+            "0x569a03bf",
+            "0xa04ef427",
+            "0x3223af9d",
+            "0xfdff9b4d",
+            "0x08ae4b0c",
+            "0xe2f67dcd",
+            "0xe54ffe8c",
+          ],
+        },
+        {
+          facetAddress: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
+          action: FacetCutAction.Add,
+          functionSelectors: ["0x95d89b41"],
+        },
+        {
+          facetAddress: AddressZero,
+          action: FacetCutAction.Remove,
+          functionSelectors: ["0x52ef6b2c", "0xf2fde38b"],
+        },
+        {
+          facetAddress: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+          action: FacetCutAction.Replace,
+          functionSelectors: ["0xcdffacc6"],
+        },
+        {
+          facetAddress: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+          action: FacetCutAction.Add,
+          functionSelectors: ["0x4290f136"],
+        },
+      ])
+    );
   });
 });
 
